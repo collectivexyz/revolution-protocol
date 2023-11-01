83ba6b58d42106062b7457b535c4209b1619817a
Will Hay <20303031+rocketmanh@users.noreply.github.com>
2023-11-01 15:19:18 -0400

    separate test files

--- test/CultureIndex.Voting.t.sol
+++ test/CultureIndex.Voting.t.sol
@@ -9,7 +9,7 @@ import {MockERC20} from "./MockERC20.sol";
  * @title CultureIndexTest
  * @dev Test contract for CultureIndex
  */
-contract CultureIndexTest is Test {
+contract CultureIndexVotingTest is Test {
     CultureIndex public cultureIndex;
     MockERC20 public mockVotingToken;
 
@@ -22,48 +22,6 @@ contract CultureIndexTest is Test {
 
         // Initialize your CultureIndex contract
         cultureIndex = new CultureIndex(address(mockVotingToken));
-    }
-
-    /**
-     * @dev Test case to validate art piece creation functionality
-     *
-     * We create a new art piece with given metadata and creators.
-     * Then we fetch the created art piece by its ID and assert
-     * its properties to ensure they match what was set.
-     */
-    function testCreatePiece() public {
-        setUp();
-
-        CultureIndex.ArtPieceMetadata memory metadata = CultureIndex
-            .ArtPieceMetadata({
-                name: "Mona Lisa",
-                description: "A masterpiece",
-                mediaType: CultureIndex.MediaType.IMAGE,
-                image: "ipfs://legends",
-                text: "",
-                animationUrl: ""
-            });
-
-        CultureIndex.CreatorBps[]
-            memory creators = new CultureIndex.CreatorBps[](1);
-        creators[0] = CultureIndex.CreatorBps({
-            creator: address(0x1),
-            bps: 10000
-        });
-
-        uint256 newPieceId = cultureIndex.createPiece(metadata, creators);
-
-        // Validate that the piece was created with correct data
-        CultureIndex.ArtPiece memory createdPiece = cultureIndex.getPieceById(
-            newPieceId
-        );
-
-        assertEq(createdPiece.id, newPieceId);
-        assertEq(createdPiece.metadata.name, "Mona Lisa");
-        assertEq(createdPiece.metadata.description, "A masterpiece");
-        assertEq(createdPiece.metadata.image, "ipfs://legends");
-        assertEq(createdPiece.creators[0].creator, address(0x1));
-        assertEq(createdPiece.creators[0].bps, 10000);
     }
 
     /**
