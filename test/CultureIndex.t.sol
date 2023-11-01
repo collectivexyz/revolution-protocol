5ec7a9bd1e67519685a7765db54796752dc80d0c
Will Hay <20303031+rocketmanh@users.noreply.github.com>
2023-11-01 14:58:10 -0400

    mock erc20 and fix prettier

--- test/CultureIndex.t.sol
+++ test/CultureIndex.t.sol
@@ -1,39 +1,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import { Test } from "forge-std/Test.sol";
-import { CultureIndex } from "../packages/revolution-contracts/CultureIndex.sol";
-import { IERC20 } from "../packages/revolution-contracts/IERC20.sol";
+import {Test} from "forge-std/Test.sol";
+import {CultureIndex} from "../packages/revolution-contracts/CultureIndex.sol";
+import {MockERC20} from "./MockERC20.sol";
 
+/**
+ * @title CultureIndexTest
+ * @dev Test contract for CultureIndex
+ */
 contract CultureIndexTest is Test {
     CultureIndex public cultureIndex;
-    IERC20 public mockVotingToken;
+    MockERC20 public mockVotingToken;
 
+    /**
+     * @dev Setup function for each test case
+     */
     function setUp() public {
         // Initialize your mock ERC20 token here, if needed
-        // mockVotingToken = new MockIERC20();
+        mockVotingToken = new MockERC20();
+
         // Initialize your CultureIndex contract
         cultureIndex = new CultureIndex(address(mockVotingToken));
     }
 
+    /**
+     * @dev Test case to validate art piece creation functionality
+     *
+     * We create a new art piece with given metadata and creators.
+     * Then we fetch the created art piece by its ID and assert
+     * its properties to ensure they match what was set.
+     */
     function testCreatePiece() public {
-        CultureIndex.ArtPieceMetadata memory metadata = CultureIndex.ArtPieceMetadata({
-            name: "Mona Lisa",
-            description: "A masterpiece",
-            mediaType: CultureIndex.MediaType.IMAGE,
-            image: "ipfs://legends",
-            text: "",
-            animationUrl: ""
-        });
+        setUp();
 
-        CultureIndex.CreatorBps[] memory creators = new CultureIndex.CreatorBps[](1);
-        creators[0] = CultureIndex.CreatorBps({ creator: address(0x1), bps: 10000 });
+        CultureIndex.ArtPieceMetadata memory metadata = CultureIndex
+            .ArtPieceMetadata({
+                name: "Mona Lisa",
+                description: "A masterpiece",
+                mediaType: CultureIndex.MediaType.IMAGE,
+                image: "ipfs://legends",
+                text: "",
+                animationUrl: ""
+            });
+
+        CultureIndex.CreatorBps[]
+            memory creators = new CultureIndex.CreatorBps[](1);
+        creators[0] = CultureIndex.CreatorBps({
+            creator: address(0x1),
+            bps: 10000
+        });
 
         uint256 newPieceId = cultureIndex.createPiece(metadata, creators);
 
         // Validate that the piece was created with correct data
-        CultureIndex.ArtPiece memory createdPiece = cultureIndex.getPieceById(newPieceId);
-
+        CultureIndex.ArtPiece memory createdPiece = cultureIndex.getPieceById(
+            newPieceId
+        );
 
         assertEq(createdPiece.id, newPieceId);
         assertEq(createdPiece.metadata.name, "Mona Lisa");
---
+++ test/MockERC20.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract MockERC20 {
+    string public name = "Mock ERC20 Token";
+    string public symbol = "MOCK";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public allowances;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+
+    constructor() {
+        // Mint some initial supply for testing purposes
+        _mint(msg.sender, 10000 * 10 ** uint256(decimals));
+    }
+
+    function balanceOf(address _owner) external view returns (uint256) {
+        return balances[_owner];
+    }
+
+    function allowance(
+        address _owner,
+        address _spender
+    ) external view returns (uint256) {
+        return allowances[_owner][_spender];
+    }
+
+    function transfer(address _to, uint256 _value) external returns (bool) {
+        require(balances[msg.sender] >= _value, "Insufficient balance");
+        balances[msg.sender] -= _value;
+        balances[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) external returns (bool) {
+        allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool) {
+        require(balances[_from] >= _value, "Insufficient balance");
+        require(allowances[_from][msg.sender] >= _value, "Allowance exceeded");
+
+        balances[_from] -= _value;
+        balances[_to] += _value;
+        allowances[_from][msg.sender] -= _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+
+    function _mint(address _to, uint256 _value) internal {
+        totalSupply += _value;
+        balances[_to] += _value;
+        emit Transfer(address(0), _to, _value);
+    }
+}
