5c0b460f55718eccd722d5f9f12d2be29c4b98ad
Will Hay <20303031+rocketmanh@users.noreply.github.com>
2023-11-10 12:16:20 -0500

    auction house tests

---
+++ test/VerbsAuctionHouse.t.sol
@@ -0,0 +1,215 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
+
+import {Test} from "forge-std/Test.sol";
+import {VerbsAuctionHouse} from "../packages/revolution-contracts/VerbsAuctionHouse.sol";
+import {MockERC20} from "./MockERC20.sol";
+import {VerbsToken} from "../packages/revolution-contracts/VerbsToken.sol";
+import {IVerbsToken} from "../packages/revolution-contracts/interfaces/IVerbsToken.sol";
+import { IProxyRegistry } from "../packages/revolution-contracts/external/opensea/IProxyRegistry.sol";
+import {VerbsDescriptor} from "../packages/revolution-contracts/VerbsDescriptor.sol";
+import {CultureIndex} from "../packages/revolution-contracts/CultureIndex.sol";
+import { IVerbsDescriptorMinimal } from "../packages/revolution-contracts/interfaces/IVerbsDescriptorMinimal.sol";
+import { ICultureIndex, ICultureIndexEvents } from "../packages/revolution-contracts/interfaces/ICultureIndex.sol";
+import { IVerbsAuctionHouse } from "../packages/revolution-contracts/interfaces/IVerbsAuctionHouse.sol";
+
+contract VerbsAuctionHouseTest is Test {
+    VerbsAuctionHouse public auctionHouse;
+    MockERC20 public mockWETH;
+    VerbsToken public verbs;
+    VerbsDescriptor public descriptor;
+    CultureIndex public cultureIndex;
+
+    function setUp() public {
+        mockWETH = new MockERC20();
+
+        // Additional setup for VerbsToken similar to VerbsTokenTest
+        ProxyRegistry _proxyRegistry = new ProxyRegistry();
+
+        CultureIndex _cultureIndex = new CultureIndex(address(mockWETH), address(this));
+        cultureIndex = _cultureIndex;
+
+        // Initialize VerbsToken with additional parameters
+        verbs = new VerbsToken(
+            address(this),             // Address of the minter (and initial owner)
+            address(this),             // Address of the owner
+            IVerbsDescriptorMinimal(address(0)),
+            _proxyRegistry,
+            ICultureIndex(address(_cultureIndex))
+        );
+
+        cultureIndex.transferOwnership(address(verbs));
+
+        // Deploy a new VerbsDescriptor, which will be used by VerbsToken
+        descriptor = new VerbsDescriptor(address(verbs));
+        IVerbsDescriptorMinimal _descriptor = descriptor;
+
+        // Set the culture index and descriptor in VerbsToken
+        verbs.setCultureIndex(ICultureIndex(address(_cultureIndex)));
+
+        auctionHouse = new VerbsAuctionHouse();
+
+        // Initialize the auction house with mock contracts and parameters
+        auctionHouse.initialize(
+            IVerbsToken(address(verbs)),
+            address(mockWETH),
+            address(this), // Owner of the auction house
+            15 minutes,    // timeBuffer
+            1 ether,       // reservePrice
+            5,             // minBidIncrementPercentage
+            24 hours       // duration
+        );
+
+        //set minter of verbstoken to be auction house
+        verbs.setMinter(address(auctionHouse));
+        verbs.lockMinter();
+    }
+
+    // Fallback function to allow contract to receive Ether
+    receive() external payable {}
+
+    function testInitializationParameters() public {
+
+        assertEq(auctionHouse.weth(), address(mockWETH), "WETH address should be set correctly");
+        assertEq(auctionHouse.timeBuffer(), 15 minutes, "Time buffer should be set correctly");
+        assertEq(auctionHouse.reservePrice(), 1 ether, "Reserve price should be set correctly");
+        assertEq(auctionHouse.minBidIncrementPercentage(), 5, "Min bid increment percentage should be set correctly");
+        assertEq(auctionHouse.duration(), 24 hours, "Auction duration should be set correctly");
+    }
+    
+
+    function testAuctionCreation() public {
+        setUp();
+        createDefaultArtPiece();
+
+        auctionHouse.unpause();
+        uint256 startTime = block.timestamp;
+
+        (uint256 verbId, uint256 amount, uint256 auctionStartTime, uint256 auctionEndTime, address payable bidder, bool settled) = auctionHouse.auction();
+        assertEq(auctionStartTime, startTime, "Auction start time should be set correctly");
+        assertEq(auctionEndTime, startTime + auctionHouse.duration(), "Auction end time should be set correctly");
+        assertEq(verbId, 0, "Auction should be for the zeroth verb");
+    }
+
+
+    function testBiddingProcess() public {
+        setUp();
+        createDefaultArtPiece();
+
+        auctionHouse.unpause();
+        uint256 bidAmount = auctionHouse.reservePrice() + 0.1 ether;
+        vm.deal(address(1), bidAmount + 1 ether);
+
+        vm.startPrank(address(1));
+        auctionHouse.createBid{value: bidAmount}(0); // Assuming the first auction's verbId is 0
+        (uint256 verbId, uint256 amount, , uint256 endTime, address payable bidder, ) = auctionHouse.auction();
+
+        assertEq(amount, bidAmount, "Bid amount should be set correctly");
+        assertEq(bidder, address(1), "Bidder address should be set correctly");
+        vm.stopPrank();
+
+        vm.warp(endTime + 1);
+        createDefaultArtPiece();
+
+        auctionHouse.settleCurrentAndCreateNewAuction(); // This will settle the current auction and create a new one
+
+        assertEq(verbs.ownerOf(verbId), address(1), "Verb should be transferred to the auction house");
+    }
+    
+    function testSettlingAuctions() public {
+        setUp();
+        createDefaultArtPiece();
+        auctionHouse.unpause();
+
+        (uint256 verbId, , , uint256 endTime, , ) = auctionHouse.auction();
+        assertEq(verbs.ownerOf(verbId), address(auctionHouse), "Verb should be transferred to the auction house");
+
+        vm.warp(endTime + 1);
+        createDefaultArtPiece();
+
+        auctionHouse.settleCurrentAndCreateNewAuction(); // This will settle the current auction and create a new one
+
+        (, , , , , bool settled) = auctionHouse.auction();
+
+        assertEq(settled, false, "Auction should not be settled because new one created");
+    }
+
+    
+
+    function testAdministrativeFunctions() public {
+        uint256 newTimeBuffer = 10 minutes;
+        auctionHouse.setTimeBuffer(newTimeBuffer);
+        assertEq(auctionHouse.timeBuffer(), newTimeBuffer, "Time buffer should be updated correctly");
+
+        uint256 newReservePrice = 2 ether;
+        auctionHouse.setReservePrice(newReservePrice);
+        assertEq(auctionHouse.reservePrice(), newReservePrice, "Reserve price should be updated correctly");
+
+        uint8 newMinBidIncrementPercentage = 10;
+        auctionHouse.setMinBidIncrementPercentage(newMinBidIncrementPercentage);
+        assertEq(auctionHouse.minBidIncrementPercentage(), newMinBidIncrementPercentage, "Min bid increment percentage should be updated correctly");
+    }
+    
+
+    function testAccessControl() public {
+        vm.startPrank(address(1));
+        vm.expectRevert();
+        auctionHouse.pause();
+        vm.stopPrank();
+
+        vm.startPrank(address(1));
+        vm.expectRevert();
+        auctionHouse.unpause();
+        vm.stopPrank();
+    }
+    
+
+    // Utility function to create a new art piece and return its ID
+    function createArtPiece(
+        string memory name,
+        string memory description,
+        ICultureIndex.MediaType mediaType,
+        string memory image,
+        string memory text,
+        string memory animationUrl,
+        address creatorAddress,
+        uint256 creatorBps
+    ) internal returns (uint256) {
+        ICultureIndex.ArtPieceMetadata memory metadata = ICultureIndex
+            .ArtPieceMetadata({
+                name: name,
+                description: description,
+                mediaType: mediaType,
+                image: image,
+                text: text,
+                animationUrl: animationUrl
+            });
+
+        ICultureIndex.CreatorBps[]
+            memory creators = new ICultureIndex.CreatorBps[](1);
+        creators[0] = ICultureIndex.CreatorBps({
+            creator: creatorAddress,
+            bps: creatorBps
+        });
+
+        return cultureIndex.createPiece(metadata, creators);
+    }
+
+    //Utility function to create default art piece
+    function createDefaultArtPiece() public returns (uint256) {
+        return createArtPiece(
+            "Mona Lisa",
+            "A masterpiece",
+            ICultureIndex.MediaType.IMAGE,
+            "ipfs://legends",
+            "",
+            "",
+            address(0x1),
+            10000
+        );
+    }
+}
+
+contract ProxyRegistry is IProxyRegistry {
+    mapping(address => address) public proxies;
+}
