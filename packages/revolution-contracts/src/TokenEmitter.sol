110be11a36cbb42022cdbd871cec2add9ce5848e
Will Hay <20303031+rocketmanh@users.noreply.github.com>
2023-11-22 21:32:24 -0500

    rm unneeded import

--- packages/revolution-contracts/src/TokenEmitter.sol
+++ packages/revolution-contracts/src/TokenEmitter.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.22;
 
 import { VRGDAC } from "./libs/VRGDAC.sol";
-import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { toDaysWadUnsafe, wadDiv, wadMul } from "./libs/SignedWadMath.sol";
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { NontransferableERC20 } from "./NontransferableERC20.sol";
