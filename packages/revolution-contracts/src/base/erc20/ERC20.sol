c74732edb5002bcd594d56e6cfff2e880de601e5
Will Hay <20303031+rocketmanh@users.noreply.github.com>
2023-11-24 15:10:45 -0500

    just use openzeppelin

--- packages/revolution-contracts/src/base/erc20/ERC20.sol
+++ packages/revolution-contracts/src/base/erc20/ERC20.sol
@@ -4,9 +4,9 @@
 pragma solidity ^0.8.22;
 
 import { Context } from "@openzeppelin/contracts/utils/Context.sol";
-import { IERC20 } from "../../interfaces/ERC20/IERC20.sol";
-import { IERC20Metadata } from "../../interfaces/ERC20/IERC20Metadata.sol";
-import { IERC20Errors } from "../../interfaces/ERC20/IERC20Errors.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC20Metadata } from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import { IERC20Errors } from "@openzeppelin/contracts/interfaces/draft-IERC6093.sol";
 
 /**
  * @dev Implementation of the {IERC20} interface.
