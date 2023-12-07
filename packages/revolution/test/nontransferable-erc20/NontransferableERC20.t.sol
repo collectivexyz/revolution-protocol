a119b5efb229fd57bd8c552ffbf1553271858e11
Will Hay <20303031+rocketmanh@users.noreply.github.com>
2023-12-07 14:03:27 -0600

    more tests

--- packages/revolution/test/nontransferable-erc20/NontransferableERC20.t.sol
+++ packages/revolution/test/nontransferable-erc20/NontransferableERC20.t.sol
@@ -53,4 +53,109 @@ contract NontransferableERC20TestSuite is Test {
         assertEq(token.allowance(owner, spender), 0, "Allowance should remain zero");
     }
 
+    function testMintingBehavior() public {
+        address account = address(0x3);
+        uint256 mintAmount = 500 * 1e18;
+
+        // Attempt minting by a non-owner
+        address nonOwner = address(0x4);
+        vm.startPrank(nonOwner);
+        vm.expectRevert();
+        token.mint(account, mintAmount);
+        vm.stopPrank();
+
+        // Verify that the balance remains unchanged
+        assertEq(token.balanceOf(account), 0, "Non-owner should not be able to mint");
+
+        // Minting by the owner
+        vm.startPrank(address(this));
+        token.mint(account, mintAmount);
+        vm.stopPrank();
+
+        // Verify balance and total supply
+        assertEq(token.balanceOf(account), mintAmount, "Minting failed to update balance");
+        assertEq(token.totalSupply(), mintAmount, "Total supply not updated correctly");
+    }
+
+    function testVotingAndDelegation() public {
+        address delegate = address(0x6);
+        uint256 mintAmount = 1000 * 1e18;
+
+        // Mint tokens to the owner
+        vm.startPrank(address(this));
+        token.mint(address(this), mintAmount);
+        vm.stopPrank();
+
+        // Delegate voting power
+        token.delegate(delegate);
+
+        // Check the voting power of the delegate
+        assertEq(token.getVotes(delegate), mintAmount, "Delegation failed to assign voting power");
+
+        // Ensure that no tokens were transferred in the process of delegation
+        assertEq(token.balanceOf(delegate), 0, "Delegation should not transfer tokens");
+    }
+
+    function testTokenMetadata() public {
+        assertEq(token.name(), "Revolution Governance", "Incorrect token name");
+        assertEq(token.symbol(), "GOV", "Incorrect token symbol");
+        assertEq(token.decimals(), 18, "Incorrect number of decimals");
+    }
+
+    function testSupplyInvariants(uint256 mintAmount1) public {
+        vm.assume(mintAmount1 < type(uint208).max);
+
+        uint256 total = mintAmount1 + mintAmount1 / 10;
+
+        vm.assume(total < type(uint208).max);
+        address account1 = address(0x7);
+        address account2 = address(0x8);
+
+
+        // Mint tokens to different accounts
+        vm.startPrank(address(this));
+        token.mint(account1, mintAmount1);
+        token.mint(account2, mintAmount1 / 10);
+        vm.stopPrank();
+
+        // Check total supply
+        uint256 totalSupply = token.totalSupply();
+
+        emit Log("totalSupply", totalSupply);
+
+        assertEq(totalSupply, mintAmount1 + mintAmount1 / 10, "Total supply should equal sum of balances");
+
+        // Check individual balances
+        assertEq(token.balanceOf(account1), mintAmount1, "Incorrect balance for account1");
+        assertEq(token.balanceOf(account2), mintAmount1 / 10, "Incorrect balance for account2");
+    }
+
+    function testAccessControl() public {
+        address nonOwner = address(0x9);
+        uint256 mintAmount = 500 * 1e18;
+
+        // Attempt to mint tokens by a non-owner
+        vm.startPrank(nonOwner);
+        vm.expectRevert();
+        token.mint(address(1), mintAmount);
+        vm.stopPrank();
+
+        // Minting by the owner
+        vm.startPrank(address(this));
+        token.mint(address(1), mintAmount);
+        vm.stopPrank();
+
+        // Verify that the minting was successful
+        assertEq(token.balanceOf(address(1)), mintAmount, "Owner should be able to mint");
+    }
+
+    function testEdgeCases() public {
+        // Minting an excessive amount of tokens (overflow check)
+        uint256 excessiveAmount = type(uint256).max;
+        vm.startPrank(address(this));
+        vm.expectRevert();
+        token.mint(address(1), excessiveAmount);
+        vm.stopPrank();
+    }
+
 }
