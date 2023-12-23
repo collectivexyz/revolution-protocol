016d9d7361a6f6ba97900c9e3de5966e97354f2f
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2023-12-22 20:54:27 -0500

    minor

--- packages/revolution/src/governance/DAOExecutor.sol
+++ packages/revolution/src/governance/DAOExecutor.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
-/// @title The Verbs DAO executo
+/// @title The Verbs DAO executor
 
 /*********************************
  * ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ *
