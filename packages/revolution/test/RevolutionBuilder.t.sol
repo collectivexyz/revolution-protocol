688fc4fb00946e048de66fe3c4920701db739b11
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-01-12 16:10:45 -0500

    clean up test files, add voting power test file

--- packages/revolution/test/RevolutionBuilder.t.sol
+++ packages/revolution/test/RevolutionBuilder.t.sol
@@ -11,6 +11,7 @@ import { IAuctionHouse, AuctionHouse } from "../src/AuctionHouse.sol";
 import { RevolutionDAOLogicV1 } from "../src/governance/RevolutionDAOLogicV1.sol";
 import { DAOExecutor } from "../src/governance/DAOExecutor.sol";
 import { CultureIndex } from "../src/culture-index/CultureIndex.sol";
+import { ICultureIndex } from "../src/interfaces/ICultureIndex.sol";
 import { RevolutionPoints } from "../src/RevolutionPoints.sol";
 import { RevolutionVotingPower } from "../src/RevolutionVotingPower.sol";
 import { RevolutionPointsEmitter } from "../src/RevolutionPointsEmitter.sol";
@@ -411,5 +412,147 @@ contract RevolutionBuilderTest is Test {
         createUsers(_numVoters, _balance);
 
         createTokens(_numVoters);
+    }
+
+    // Utility function to create a new art piece and return its ID
+    function createArtPiece(
+        string memory name,
+        string memory description,
+        ICultureIndex.MediaType mediaType,
+        string memory image,
+        string memory text,
+        string memory animationUrl,
+        address creatorAddress,
+        uint256 creatorBps
+    ) internal returns (uint256) {
+        address[] memory creatorAddresses = new address[](1);
+        creatorAddresses[0] = creatorAddress;
+
+        uint256[] memory creatorBpsArray = new uint256[](1);
+        creatorBpsArray[0] = creatorBps;
+
+        return
+            createArtPieceMultiCreator(
+                name,
+                description,
+                mediaType,
+                image,
+                text,
+                animationUrl,
+                creatorAddresses,
+                creatorBpsArray
+            );
+    }
+
+    //Utility function to create default art piece
+    function createDefaultArtPiece() public returns (uint256) {
+        return
+            createArtPiece(
+                "Mona Lisa",
+                "A masterpiece",
+                ICultureIndex.MediaType.IMAGE,
+                "ipfs://legends",
+                "",
+                "",
+                address(0x1),
+                10000
+            );
+    }
+
+    // Utility function to create a new art piece with multiple creators and return its ID
+    function createArtPieceMultiCreator(
+        string memory name,
+        string memory description,
+        ICultureIndex.MediaType mediaType,
+        string memory image,
+        string memory text,
+        string memory animationUrl,
+        address[] memory creatorAddresses,
+        uint256[] memory creatorBps
+    ) internal returns (uint256) {
+        ICultureIndex.ArtPieceMetadata memory metadata = ICultureIndex.ArtPieceMetadata({
+            name: name,
+            description: description,
+            mediaType: mediaType,
+            image: image,
+            text: text,
+            animationUrl: animationUrl
+        });
+
+        ICultureIndex.CreatorBps[] memory creators = new ICultureIndex.CreatorBps[](creatorAddresses.length);
+        for (uint256 i = 0; i < creatorAddresses.length; i++) {
+            creators[i] = ICultureIndex.CreatorBps({ creator: creatorAddresses[i], bps: creatorBps[i] });
+        }
+
+        return cultureIndex.createPiece(metadata, creators);
+    }
+
+    //function to create basic metadata
+    function createDefaultMetadata() internal pure returns (ICultureIndex.ArtPieceMetadata memory) {
+        return
+            ICultureIndex.ArtPieceMetadata({
+                name: "Mona Lisa",
+                description: "A masterpiece",
+                mediaType: ICultureIndex.MediaType.IMAGE,
+                image: "ipfs://legends",
+                text: "",
+                animationUrl: ""
+            });
+    }
+
+    // Function to create ArtPieceMetadata
+    function createArtPieceMetadata(
+        string memory name,
+        string memory description,
+        ICultureIndex.MediaType mediaType,
+        string memory image,
+        string memory text,
+        string memory animationUrl
+    ) public pure returns (CultureIndex.ArtPieceMetadata memory) {
+        // <-- Change visibility and mutability as needed
+        ICultureIndex.ArtPieceMetadata memory metadata = ICultureIndex.ArtPieceMetadata({
+            name: name,
+            description: description,
+            mediaType: mediaType,
+            image: image,
+            text: text,
+            animationUrl: animationUrl
+        });
+        return metadata;
+    }
+
+    // Function to create CreatorBps array
+    function createArtPieceCreators(
+        address creatorAddress,
+        uint256 creatorBps
+    ) public pure returns (CultureIndex.CreatorBps[] memory) {
+        // <-- Change visibility and mutability as needed
+        ICultureIndex.CreatorBps[] memory creators = new ICultureIndex.CreatorBps[](1);
+        creators[0] = ICultureIndex.CreatorBps({ creator: creatorAddress, bps: creatorBps });
+        return creators;
+    }
+
+    //returns metadata and creators in a tuple
+    function createArtPieceTuple(
+        string memory name,
+        string memory description,
+        ICultureIndex.MediaType mediaType,
+        string memory image,
+        string memory text,
+        string memory animationUrl,
+        address creatorAddress,
+        uint256 creatorBps
+    ) public pure returns (CultureIndex.ArtPieceMetadata memory, ICultureIndex.CreatorBps[] memory) {
+        // <-- Change here
+        ICultureIndex.ArtPieceMetadata memory metadata = createArtPieceMetadata(
+            name,
+            description,
+            mediaType,
+            image,
+            text,
+            animationUrl
+        );
+        ICultureIndex.CreatorBps[] memory creators = createArtPieceCreators(creatorAddress, creatorBps);
+        return (metadata, creators);
     }
 }
--- packages/revolution/test/voting-power/VotingPower.t.sol
+++ packages/revolution/test/voting-power/VotingPower.t.sol
@@ -8,6 +8,10 @@ import { IRevolutionBuilder } from "../../src/interfaces/IRevolutionBuilder.sol"
 import { Ownable2StepUpgradeable } from "@openzeppelin/contracts-upgradeable/access/Ownable2StepUpgradeable.sol";
 import { ERC1967Proxy } from "../../src/libs/proxy/ERC1967Proxy.sol";
 
+import { IRevolutionVotingPower } from "../../src/interfaces/IRevolutionVotingPower.sol";
+import { IRevolutionToken } from "../../src/interfaces/IRevolutionToken.sol";
+import { ICultureIndex } from "../../src/interfaces/ICultureIndex.sol";
+
 contract VotingPowerTest is RevolutionBuilderTest {
     event Log(string, uint);
 
@@ -25,5 +29,73 @@ contract VotingPowerTest is RevolutionBuilderTest {
         bytes32 salt = bytes32(uint256(uint160(revolutionToken)) << 96);
 
         address revolutionVotingPower = address(new ERC1967Proxy{ salt: salt }(revolutionVotingPowerImpl, ""));
+
+        vm.startPrank(address(manager));
+        IRevolutionVotingPower(revolutionVotingPower).initialize({
+            initialOwner: address(executor),
+            revolutionPoints: address(revolutionPoints),
+            revolutionPointsVoteWeight: revolutionVotingPowerParams.revolutionPointsVoteWeight,
+            revolutionToken: revolutionToken,
+            revolutionTokenVoteWeight: revolutionVotingPowerParams.revolutionTokenVoteWeight
+        });
+    }
+
+    function test__DefaultVotingPowerCalculation(
+        uint256 pointsBalance,
+        uint256 pointsVoteWeight,
+        uint256 tokenVoteWeight,
+        uint256 tokenBalance
+    ) public {
+        pointsBalance = bound(pointsBalance, 1, 1e18);
+        pointsVoteWeight = bound(pointsVoteWeight, 1 * 1e18, 1_000_000 * 1e18);
+        tokenVoteWeight = bound(tokenVoteWeight, 1 * 1e18, 1_000_000 * 1e18);
+        tokenBalance = bound(tokenBalance, 1, 1e4);
+
+        //mint points and token to address voter
+        address voter = address(this);
+
+        for (uint256 i = 0; i < tokenBalance; i++) {
+            createDefaultArtPiece();
+        }
+
+        vm.roll(vm.getBlockNumber() + 1);
+
+        for (uint256 i = 0; i < tokenBalance; i++) {
+            vm.prank(address(auction));
+            revolutionToken.mint();
+
+            //transfer to voter
+            vm.prank(address(auction));
+            revolutionToken.transferFrom(address(auction), voter, i);
+        }
+
+        bytes32 salt = bytes32(uint256(uint160(address(revolutionToken))) << 96);
+
+        address revolutionVotingPower = address(new ERC1967Proxy{ salt: salt }(revolutionVotingPowerImpl, ""));
+
+        // Arrange: Set up token balances and vote weights
+        uint256 tokenBalance = 1;
+
+        emit log_address(revolutionToken.minter());
+
+        vm.prank(address(revolutionPointsEmitter));
+        revolutionPoints.mint(voter, pointsBalance);
+
+        // Act: Calculate the expected and actual voting power
+        uint256 expectedVotingPower = (pointsBalance * pointsVoteWeight) + (tokenBalance * tokenVoteWeight);
+
+        vm.startPrank(address(manager));
+        IRevolutionVotingPower(revolutionVotingPower).initialize({
+            initialOwner: address(executor),
+            revolutionPoints: address(revolutionPoints),
+            revolutionPointsVoteWeight: pointsVoteWeight,
+            revolutionToken: address(revolutionToken),
+            revolutionTokenVoteWeight: tokenVoteWeight
+        });
+
+        uint256 actualVotingPower = IRevolutionVotingPower(revolutionVotingPower).getVotes(voter);
+
+        // Assert: The actual voting power should match the expected voting power
+        assertEq(actualVotingPower, expectedVotingPower);
     }
 }
