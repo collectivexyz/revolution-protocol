d3d46b2909304e44ff496d6b5c73db4198965c78
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-01-15 19:45:53 -0500

    verbId -> tokenId

--- packages/revolution/src/interfaces/IAuctionHouse.sol
+++ packages/revolution/src/interfaces/IAuctionHouse.sol
@@ -68,7 +68,7 @@ interface IAuctionHouse {
 
     struct Auction {
         // ID for the Verb (ERC721 token ID)
-        uint256 verbId;
+        uint256 tokenId;
         // The current highest bid amount
         uint256 amount;
         // The time that the auction started
@@ -83,13 +83,13 @@ interface IAuctionHouse {
         bool settled;
     }
 
-    event AuctionCreated(uint256 indexed verbId, uint256 startTime, uint256 endTime);
+    event AuctionCreated(uint256 indexed tokenId, uint256 startTime, uint256 endTime);
 
-    event AuctionBid(uint256 indexed verbId, address bidder, address sender, uint256 value, bool extended);
+    event AuctionBid(uint256 indexed tokenId, address bidder, address sender, uint256 value, bool extended);
 
-    event AuctionExtended(uint256 indexed verbId, uint256 endTime);
+    event AuctionExtended(uint256 indexed tokenId, uint256 endTime);
 
-    event AuctionSettled(uint256 indexed verbId, address winner, uint256 amount, uint256 creatorTokensEmitted);
+    event AuctionSettled(uint256 indexed tokenId, address winner, uint256 amount, uint256 creatorTokensEmitted);
 
     event AuctionTimeBufferUpdated(uint256 timeBuffer);
 
@@ -107,7 +107,7 @@ interface IAuctionHouse {
 
     function settleCurrentAndCreateNewAuction() external;
 
-    function createBid(uint256 verbId, address bidder, address referral) external payable;
+    function createBid(uint256 tokenId, address bidder, address referral) external payable;
 
     function pause() external;
 
