81fb272cb11e087557f63b5d00c6de8d8a62086a
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-02-07 10:51:20 -0500

    fix

--- packages/splits/src/interfaces/IPointsEmitterLike.sol
+++ packages/splits/src/interfaces/IPointsEmitterLike.sol
@@ -4,6 +4,12 @@ pragma solidity ^0.8.22;
 import { IRewardSplits } from "@cobuild/protocol-rewards/src/abstract/RewardSplits.sol";
 
 interface IRevolutionPointsEmitter is IRewardSplits {
+    struct ProtocolRewardAddresses {
+        address builder;
+        address purchaseReferral;
+        address deployer;
+    }
+
     function buyToken(
         address[] calldata addresses,
         uint[] calldata bps,
