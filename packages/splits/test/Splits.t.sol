58170b4432dda46fd1470f9a788842a5444449df
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-02-07 00:09:33 -0500

    blank test

--- packages/splits/test/Splits.t.sol
+++ packages/splits/test/Splits.t.sol
@@ -2,13 +2,18 @@
 pragma solidity ^0.8.22;
 
 import { Test } from "forge-std/Test.sol";
+import { SplitMain } from "../src/SplitMain.sol";
 
 contract SplitsTest is Test {
     address splitMain;
 
     address revolutionPoints = address(0x1);
 
-    function setUp() public override {
+    function setUp() public {
         splitMain = address(new SplitMain(revolutionPoints));
+    }
+
+    function test__Blank() public {
+        assertTrue(true);
     }
 }
