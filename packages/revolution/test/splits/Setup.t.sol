09079e2558afa393a440ec309752318b14999d8a
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-02-11 14:15:07 -0500

    fix cyclic dep

--- packages/revolution/test/splits/Setup.t.sol
+++ packages/revolution/test/splits/Setup.t.sol
@@ -2,18 +2,18 @@
 pragma solidity ^0.8.22;
 
 import { Test } from "forge-std/Test.sol";
-import { SplitMain } from "../src/SplitMain.sol";
-import { SplitWallet } from "../src/SplitWallet.sol";
-import { ISplitMain } from "../src/interfaces/ISplitMain.sol";
+import { SplitMain } from "@cobuild/splits/src/SplitMain.sol";
+import { SplitWallet } from "@cobuild/splits/src/SplitWallet.sol";
+import { ISplitMain } from "@cobuild/splits/src/interfaces/ISplitMain.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC1967Proxy } from "@cobuild/utility-contracts/src/proxy/ERC1967Proxy.sol";
-import { SplitsTest } from "./Splits.t.sol";
-import { IRevolutionBuilder } from "@cobuild/revolution/src/interfaces/IRevolutionBuilder.sol";
-import { IRevolutionPointsEmitter } from "@cobuild/revolution/src/interfaces/IRevolutionPointsEmitter.sol";
-import { IRevolutionPoints } from "@cobuild/revolution/src/interfaces/IRevolutionPoints.sol";
-import { IVRGDAC } from "@cobuild/revolution/src/interfaces/IVRGDAC.sol";
+import { SplitsTest } from "./RevolutionSplits.t.sol";
+import { IRevolutionBuilder } from "../../src/interfaces/IRevolutionBuilder.sol";
+import { IRevolutionPointsEmitter } from "../../src/interfaces/IRevolutionPointsEmitter.sol";
+import { IRevolutionPoints } from "../../src/interfaces/IRevolutionPoints.sol";
+import { IVRGDAC } from "../../src/interfaces/IVRGDAC.sol";
 
-contract SplitsSetupTest is SplitsTest {
+contract SplitsTestSetupTest is SplitsTest {
     // test setup of our points emitter
     function test__BuyToken() public {
         // assert minter of points is pointsEmitter
--- packages/revolution/test/splits/SplitsCreation.t.sol
+++ packages/revolution/test/splits/SplitsCreation.t.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.22;
 
 import { Test } from "forge-std/Test.sol";
-import { SplitMain } from "../src/SplitMain.sol";
-import { SplitWallet } from "../src/SplitWallet.sol";
-import { ISplitMain } from "../src/interfaces/ISplitMain.sol";
+import { SplitMain } from "@cobuild/splits/src/SplitMain.sol";
+import { SplitWallet } from "@cobuild/splits/src/SplitWallet.sol";
+import { ISplitMain } from "@cobuild/splits/src/interfaces/ISplitMain.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { ERC1967Proxy } from "@cobuild/utility-contracts/src/proxy/ERC1967Proxy.sol";
-import { SplitsTest } from "./Splits.t.sol";
-import { IRevolutionBuilder } from "@cobuild/revolution/src/interfaces/IRevolutionBuilder.sol";
-import { IRevolutionPointsEmitter } from "@cobuild/revolution/src/interfaces/IRevolutionPointsEmitter.sol";
-import { IRevolutionPoints } from "@cobuild/revolution/src/interfaces/IRevolutionPoints.sol";
+import { SplitsTest } from "./RevolutionSplits.t.sol";
+import { IRevolutionBuilder } from "../../src/interfaces/IRevolutionBuilder.sol";
+import { IRevolutionPointsEmitter } from "../../src/interfaces/IRevolutionPointsEmitter.sol";
+import { IRevolutionPoints } from "../../src/interfaces/IRevolutionPoints.sol";
 
 contract CreateSplitsTest is SplitsTest {
     function test__CreateBasicSplit__EthPayouts() public {
