e64625d95d330e41648be8c7e88947aa5e5f1e99
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-02-11 11:02:30 -0500

    first set of basic tests

--- packages/splits/test/Splits.t.sol
+++ packages/splits/test/Splits.t.sol
@@ -3,17 +3,101 @@ pragma solidity ^0.8.22;
 
 import { Test } from "forge-std/Test.sol";
 import { SplitMain } from "../src/SplitMain.sol";
+import { ISplitMain } from "../src/interfaces/ISplitMain.sol";
 
 contract SplitsTest is Test {
-    address splitMain;
+    SplitMain splits;
 
     address revolutionPoints = address(0x1);
 
     function setUp() public {
-        splitMain = address(new SplitMain(revolutionPoints));
+        splits = new SplitMain(revolutionPoints);
     }
 
-    function test__Blank() public {
-        assertTrue(true);
+    function test__Revert_NoTreasurySplit() public {
+        address[] memory accounts = new address[](1);
+        accounts[0] = address(this);
+        uint32[] memory percentAllocations = new uint32[](1);
+        percentAllocations[0] = 1e6; // 100% allocation
+        uint32 distributorFee = 0;
+        address controller = address(this);
+
+        SplitMain.PointsData memory pointsData = ISplitMain.PointsData({
+            percentOfEther: 0,
+            accounts: new address[](0),
+            percentAllocations: new uint32[](0)
+        });
+
+        bytes4 selector = bytes4(keccak256("InvalidSplit__InvalidPointsPercent(uint32)"));
+
+        vm.expectRevert(abi.encodeWithSelector(selector, 0));
+        address split = splits.createSplit(pointsData, accounts, percentAllocations, distributorFee, controller);
+    }
+
+    function test__Revert_TooFewSplitAccounts() public {
+        address[] memory accounts = new address[](1);
+        accounts[0] = address(this);
+
+        uint32[] memory percentAllocations = new uint32[](1);
+        percentAllocations[0] = 1e6 / 2; // 100% allocation
+        uint32 distributorFee = 0;
+        address controller = address(this);
+
+        SplitMain.PointsData memory pointsData = ISplitMain.PointsData({
+            percentOfEther: 1e6 / 2,
+            accounts: accounts,
+            percentAllocations: percentAllocations
+        });
+
+        bytes4 selector = bytes4(keccak256("InvalidSplit__TooFewAccounts(uint256)"));
+
+        vm.expectRevert(abi.encodeWithSelector(selector, 0));
+        address split = splits.createSplit(
+            pointsData,
+            new address[](0),
+            percentAllocations,
+            distributorFee,
+            controller
+        );
+    }
+
+    function test__Revert_TooFewPointsAccounts() public {
+        address[] memory accounts = new address[](1);
+        accounts[0] = address(this);
+        uint32[] memory percentAllocations = new uint32[](1);
+        percentAllocations[0] = 1e6 / 2; // 100% allocation
+        uint32 distributorFee = 0;
+        address controller = address(this);
+
+        SplitMain.PointsData memory pointsData = ISplitMain.PointsData({
+            percentOfEther: 1e6 / 2,
+            accounts: new address[](0),
+            percentAllocations: new uint32[](0)
+        });
+
+        bytes4 selector = bytes4(keccak256("InvalidSplit__TooFewAccounts(uint256)"));
+
+        vm.expectRevert(abi.encodeWithSelector(selector, 0));
+        address split = splits.createSplit(pointsData, accounts, percentAllocations, distributorFee, controller);
+    }
+
+    function test__Revert_LargeTreasurySplit() public {
+        address[] memory accounts = new address[](1);
+        accounts[0] = address(this);
+        uint32[] memory percentAllocations = new uint32[](1);
+        percentAllocations[0] = 1; // 100% allocation
+        uint32 distributorFee = 0;
+        address controller = address(this);
+
+        SplitMain.PointsData memory pointsData = ISplitMain.PointsData({
+            percentOfEther: 1e6 + 1,
+            accounts: accounts,
+            percentAllocations: percentAllocations
+        });
+
+        bytes4 selector = bytes4(keccak256("InvalidSplit__InvalidAllocationsSum(uint32)"));
+
+        vm.expectRevert(abi.encodeWithSelector(selector, 1e6 + 2));
+        address split = splits.createSplit(pointsData, accounts, percentAllocations, distributorFee, controller);
     }
 }
