dfa4902edfad83102e79ef70acfa6b0cf181090f
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-02-13 12:24:23 -0500

    add back basic tests to protocol-rewards repo

--- packages/protocol-rewards/test/ProtocolRewardsTest.sol
+++ packages/protocol-rewards/test/ProtocolRewardsTest.sol
@@ -4,27 +4,53 @@ pragma solidity 0.8.22;
 import "forge-std/Test.sol";
 
 import "../src/ProtocolRewards.sol";
+import { RevolutionRewards } from "../src/abstract/RevolutionRewards.sol";
 
 contract ProtocolRewardsTest is Test {
     uint256 internal constant ETH_SUPPLY = 120_200_000 ether;
 
     ProtocolRewards internal protocolRewards;
 
+    RewardsTest internal rewardsTest;
+
     address internal collector;
     address internal builderReferral;
     address internal purchaseReferral;
     address internal deployer;
-    address internal revolution;
+    address internal revolutionDAO;
 
     function setUp() public virtual {
-        protocolRewards = new ProtocolRewards();
-
-        vm.label(address(protocolRewards), "protocolRewards");
-
         collector = makeAddr("collector");
         builderReferral = makeAddr("builderReferral");
         purchaseReferral = makeAddr("purchaseReferral");
         deployer = makeAddr("firstMinter");
-        revolution = makeAddr("revolution");
+        revolutionDAO = makeAddr("revolutionDAO");
+
+        protocolRewards = new ProtocolRewards();
+
+        rewardsTest = new RewardsTest(address(protocolRewards), address(revolutionDAO));
+
+        vm.label(address(protocolRewards), "protocolRewards");
+    }
+}
+
+contract RewardsTest is RevolutionRewards {
+    /// @param _protocolRewards The protocol rewards contract address
+    /// @param _protocolFeeRecipient The protocol fee recipient address
+    constructor(
+        address _protocolRewards,
+        address _protocolFeeRecipient
+    ) payable RevolutionRewards(_protocolRewards, _protocolFeeRecipient) {
+        if (_protocolRewards == address(0)) revert();
+        if (_protocolFeeRecipient == address(0)) revert();
+    }
+
+    function buyAndIssueRewards(
+        address builder,
+        address purchaseReferral,
+        address deployer
+    ) public payable returns (uint256 tokensSoldWad) {
+        // Calculate value left after sharing protocol rewards
+        uint256 msgValueRemaining = _handleRewardsAndGetValueToSend(msg.value, builder, purchaseReferral, deployer);
     }
 }
