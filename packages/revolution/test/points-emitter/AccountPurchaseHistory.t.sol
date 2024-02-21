916ffab7da993ec68d7f39b40778618161e6c81e
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-02-21 18:32:57 -0500

    test for grants rate 10k bps - and subsequent purchases update purchaseHistory correctly

--- packages/revolution/test/points-emitter/AccountPurchaseHistory.t.sol
+++ packages/revolution/test/points-emitter/AccountPurchaseHistory.t.sol
@@ -99,4 +99,144 @@ contract AccountPurchaseHistoryTest is PointsEmitterTest {
             "averagePurchaseBlockWad should be the current block"
         );
     }
+
+    // test that only 0 values are saved to the account purchase history if founder rate is super high
+    function test__PurchaseHistoryZeroesGrantsRateHigh() public {
+        uint256 value = 1 ether;
+        address[] memory recipients = new address[](1);
+        recipients[0] = address(1);
+
+        uint256[] memory bps = new uint256[](1);
+        bps[0] = 10_000;
+
+        vm.deal(address(0), 100000 ether);
+
+        //ensure getAccountPurchaseHistory returns the correct amount
+        IRevolutionPointsEmitter.AccountPurchaseHistory memory accountPurchaseHistory0 = revolutionPointsEmitter
+            .getAccountPurchaseHistory(address(1));
+
+        // assert averagePurchaseBlockWad is 0
+        assertEq(accountPurchaseHistory0.averagePurchaseBlockWad, 0, "averagePurchaseBlockWad should be 0");
+        assertEq(accountPurchaseHistory0.amountPaidToOwner, 0, "amountPaidToOwner should be 0");
+        assertEq(accountPurchaseHistory0.tokensBought, 0, "tokensBought should be 0");
+
+        uint256 msgValueRemaining = value - revolutionPointsEmitter.computeTotalReward(value);
+
+        uint256 amountPaidToOwner = calculateAmountForBuyers(msgValueRemaining);
+        uint256 founderGovernancePayment = calculateFounderGovernancePayment(msgValueRemaining);
+
+        int256 expectedFounderPoints = revolutionPointsEmitter.getTokenQuoteForEther(founderGovernancePayment);
+
+        int256 expectedBuyerAmount = getTokenQuoteForEtherHelper(amountPaidToOwner, expectedFounderPoints);
+
+        // set founderRateBps to 10000
+        vm.startPrank(address(executor));
+        revolutionPointsEmitter.setGrantsRateBps(10_000 - revolutionPointsEmitter.founderRateBps());
+        vm.stopPrank();
+
+        revolutionPointsEmitter.buyToken{ value: value }(
+            recipients,
+            bps,
+            IRevolutionPointsEmitter.ProtocolRewardAddresses({
+                builder: address(0),
+                purchaseReferral: address(0),
+                deployer: address(0)
+            })
+        );
+
+        //ensure getAccountPurchaseHistory returns the correct amount
+        IRevolutionPointsEmitter.AccountPurchaseHistory memory accountPurchaseHistory = revolutionPointsEmitter
+            .getAccountPurchaseHistory(address(1));
+
+        // assert amount paid to owner is 0
+        assertEq(accountPurchaseHistory.amountPaidToOwner, 0, "amountPaidToOwner should be 0");
+
+        // assert tokensBought is 0
+        assertEq(accountPurchaseHistory.tokensBought, 0, "tokensBought should be 0");
+
+        // assert averagePurchaseBlock
+        assertEq(accountPurchaseHistory.averagePurchaseBlockWad, 0, "averagePurchaseBlockWad should be 0");
+    }
+
+    // test that purchase account history is saved after multiple purchases for the same account
+    function test__PurchaseHistorySavedMultiplePurchases() public {
+        // purchase for the first account
+        uint256 value = 1 ether;
+
+        address[] memory recipients = new address[](1);
+        recipients[0] = address(1); // Assuming address(1) is the recipient for simplicity
+
+        uint[] memory bps = new uint[](1);
+        bps[0] = 10_000; // Assigning all tokens to the single recipient
+
+        IRevolutionPointsEmitter.ProtocolRewardAddresses memory protocolRewardAddresses = IRevolutionPointsEmitter
+            .ProtocolRewardAddresses({ builder: address(0), purchaseReferral: address(0), deployer: address(0) });
+
+        uint256 msgValueRemaining = value - revolutionPointsEmitter.computeTotalReward(value);
+
+        uint256 amountPaidToOwner = calculateAmountForBuyers(msgValueRemaining);
+
+        int256 expectedFounderPoints = revolutionPointsEmitter.getTokenQuoteForEther(
+            calculateFounderGovernancePayment(msgValueRemaining)
+        );
+
+        int256 expectedBuyerAmount = getTokenQuoteForEtherHelper(amountPaidToOwner, expectedFounderPoints);
+
+        // First purchase
+        revolutionPointsEmitter.buyToken{ value: value }(recipients, bps, protocolRewardAddresses);
+
+        uint256 firstBlockNumber = vm.getBlockNumber();
+
+        vm.roll(firstBlockNumber + 1e2);
+
+        // Second purchase, different value
+        uint256 secondValue = 2 ether;
+
+        uint256 msgValueRemaining2 = secondValue - revolutionPointsEmitter.computeTotalReward(secondValue);
+
+        uint256 amountPaidToOwner2 = calculateAmountForBuyers(msgValueRemaining2);
+
+        int256 expectedFounderPoints2 = revolutionPointsEmitter.getTokenQuoteForEther(
+            calculateFounderGovernancePayment(msgValueRemaining2)
+        );
+
+        int256 expectedBuyerAmount2 = getTokenQuoteForEtherHelper(
+            amountPaidToOwner2,
+            expectedFounderPoints2 + int256(revolutionPoints.totalSupply())
+        );
+
+        revolutionPointsEmitter.buyToken{ value: secondValue }(recipients, bps, protocolRewardAddresses);
+
+        // Check the account purchase history after multiple purchases
+        IRevolutionPointsEmitter.AccountPurchaseHistory memory accountPurchaseHistory = revolutionPointsEmitter
+            .getAccountPurchaseHistory(address(1));
+
+        // assert amount paid to owner is > 0 and correct
+        assertGt(accountPurchaseHistory.amountPaidToOwner, 0, "amountPaidToOwner should be greater than 0");
+        assertEq(
+            accountPurchaseHistory.amountPaidToOwner,
+            amountPaidToOwner + amountPaidToOwner2,
+            "amountPaidToOwner should be correct"
+        );
+
+        // assert tokensBought
+        assertGt(accountPurchaseHistory.tokensBought, 0, "tokensBought should be greater than 0");
+        assertEq(
+            accountPurchaseHistory.tokensBought,
+            uint256(expectedBuyerAmount + expectedBuyerAmount2),
+            "tokensBought should be correct"
+        );
+
+        // assert averagePurchaseBlockWad is > 0
+        assertGt(accountPurchaseHistory.averagePurchaseBlockWad, 0, "averagePurchaseBlockWad should be greater than 0");
+
+        // calculate the average block number
+        uint256 averageBlock = (firstBlockNumber * amountPaidToOwner + vm.getBlockNumber() * amountPaidToOwner2) /
+            (amountPaidToOwner + amountPaidToOwner2);
+        assertEq(
+            accountPurchaseHistory.averagePurchaseBlockWad,
+            averageBlock,
+            "averagePurchaseBlockWad should be correct"
+        );
+    }
 }
