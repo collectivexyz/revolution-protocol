c7373b0e2ec73122146c91f749dfc96ac4984d32
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-02-26 20:38:51 -0500

    minor test

--- packages/revolution/test/descriptor/UriData.t.sol
+++ packages/revolution/test/descriptor/UriData.t.sol
@@ -165,6 +165,36 @@ contract DescriptorURIDataTest is DescriptorTest {
         );
     }
 
+    function testMaliciousDescription() public {
+        string
+            memory description = 'This is a test cases: "Embedded Quotes", \\Double Backslashes\\, \nNewline, \rCarriage Return, \tTab, \\bBackspace, \\fForm Feed, http://example.com/?param=value&another=value, \\u{1F600} Emoji, \u0000Null Byte, \' and Control Characters like \u001F.';
+        ICultureIndex.ArtPieceMetadata memory metadata = ICultureIndex.ArtPieceMetadata({
+            name: "",
+            description: description,
+            mediaType: ICultureIndex.MediaType.IMAGE,
+            image: "https://example.com/image.png",
+            text: "",
+            animationUrl: ""
+        });
+
+        string memory uri = descriptor.dataURI(1, metadata);
+
+        assertTrue(bytes(uri).length > 0, "dataURI should not be empty");
+
+        // Check if the string contains the base64 identifier which indicates a base64 encoded data URI
+        assertEq(
+            substring(bytes(uri), 0, 29),
+            "data:application/json;base64,",
+            "dataURI should start with 'data:application/json;base64,'"
+        );
+
+        // parseJson and ensure the description is escaped
+        string memory metadataJson = decodeMetadata(uri);
+
+        //ensure doesn't revert
+        parseJson(metadataJson);
+    }
+
     /// @notice Test `genericDataURI` returns valid base64 encoded data URI
     function testGenericDataURI() public {
         ICultureIndex.ArtPieceMetadata memory metadata = ICultureIndex.ArtPieceMetadata({
