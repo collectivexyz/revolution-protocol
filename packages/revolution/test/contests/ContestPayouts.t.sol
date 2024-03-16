3018957935338c23661665320f32b779acc0dc4b
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-03-16 18:02:35 -0400

    multi creator and multi winner basic tests

--- packages/revolution/test/contests/ContestPayouts.t.sol
+++ packages/revolution/test/contests/ContestPayouts.t.sol
@@ -18,13 +18,12 @@ contract ContestOwnerControl is ContestBuilderTest {
      */
     function setUp() public virtual override {
         super.setUp();
-
-        super.setMockContestParams();
-
-        super.deployContestMock();
     }
 
     function test__ContestPayoutRevertIfNotOver() public {
+        super.setMockContestParams();
+
+        super.deployContestMock();
         vm.stopPrank();
 
         uint256 pieceId = createDefaultSubmission();
@@ -59,6 +58,10 @@ contract ContestOwnerControl is ContestBuilderTest {
     }
 
     function test__WinnerSplitReceivesCorrectETHAmount() public {
+        super.setMockContestParams();
+
+        super.deployContestMock();
+
         vm.stopPrank();
 
         // Create a default submission and set it as the winner
@@ -104,6 +107,194 @@ contract ContestOwnerControl is ContestBuilderTest {
 
         // ensure payoutIndex is 1
         assertEq(baseContest.payoutIndex(), 1, "Payout index should be 1");
+    }
+
+    function test__MultiWinnerSplitReceivesCorrectETHAmount() public {
+        super.setMockContestParams();
+
+        uint256[] memory payoutSplits = new uint256[](3);
+        // Scaled by 1e6
+        payoutSplits[0] = 500000; // 50%
+        payoutSplits[1] = 300000; // 30%
+        payoutSplits[2] = 200000; // 20%
+
+        super.setBaseContestParams(500000, block.timestamp + 60 * 60 * 24 * 7, payoutSplits);
+
+        super.deployContestMock();
+        vm.stopPrank();
+
+        // Create three default submissions and set them as the winners
+        uint256 pieceId = createDefaultSubmission();
+        uint256 pieceId2 = createContestSubmission(
+            "Second Submission",
+            "Second masterpiece",
+            ICultureIndex.MediaType.IMAGE,
+            "ipfs://second",
+            "",
+            "",
+            address(0x2),
+            10000
+        );
+        uint256 pieceId3 = createContestSubmission(
+            "Third Submission",
+            "Third masterpiece",
+            ICultureIndex.MediaType.IMAGE,
+            "ipfs://third",
+            "",
+            "",
+            address(0x3),
+            10000
+        );
+
+        // Allocate ETH to the contest contract to simulate prize pool using vm.deal
+        uint256 prizePoolAmount = 1 ether;
+        vm.deal(address(baseContest), prizePoolAmount);
+
+        // Fast forward time to after the contest ends
+        vm.warp(baseContest.endTime() + 1);
+
+        // unpause
+        vm.prank(founder);
+        baseContest.unpause();
+
+        // Calculate expected balances based on payout splits
+        uint256 expectedWinner1Balance = (prizePoolAmount * payoutSplits[0]) / 1e6;
+        uint256 expectedWinner2Balance = (prizePoolAmount * payoutSplits[1]) / 1e6;
+        uint256 expectedWinner3Balance = (prizePoolAmount * payoutSplits[2]) / 1e6;
+
+        // Pay out winners by the owner after contest ends
+        vm.prank(founder);
+        vm.expectEmit(false, true, false, true);
+        emit ReceiveETH(address(baseContest), expectedWinner1Balance);
+        vm.expectEmit(false, true, false, true);
+        emit ReceiveETH(address(baseContest), expectedWinner2Balance);
+        vm.expectEmit(false, true, false, true);
+        emit ReceiveETH(address(baseContest), expectedWinner3Balance);
+        baseContest.payOutWinners(3);
+
+        // Assert that contest balance is empty
+        assertEq(address(baseContest).balance, 0, "Contest balance should be empty");
+
+        // assert contest paid out and expect revert if trying to pay out again
+        assertEq(baseContest.paidOut(), true, "Contest should be paid out");
+
+        vm.prank(founder);
+        vm.expectRevert(IBaseContest.CONTEST_ALREADY_PAID_OUT.selector);
+        baseContest.payOutWinners(1);
+
+        // check that cultureindex maxheap.size is 0 after paying out
+        CultureIndex contestIndex = CultureIndex(address(baseContest.cultureIndex()));
+        MaxHeap maxHeap = MaxHeap(address(contestIndex.maxHeap()));
+
+        // ensure baseContest initialBalance is 1 ether
+        assertEq(baseContest.initialBalance(), 1 ether, "Initial balance should be 1 ether");
+
+        // ensure payoutIndex is 1
+        assertEq(baseContest.payoutIndex(), 3, "Payout index should be 1");
+    }
+
+    function test__MultiCreatorSplitReceivesCorrectETHAmount() public {
+        super.setMockContestParams();
+
+        uint256[] memory payoutSplits = new uint256[](3);
+        // Scaled by 1e6
+        payoutSplits[0] = 500000; // 50%
+        payoutSplits[1] = 300000; // 30%
+        payoutSplits[2] = 200000; // 20%
+
+        super.setBaseContestParams(500000, block.timestamp + 60 * 60 * 24 * 7, payoutSplits);
+
+        super.deployContestMock();
+        vm.stopPrank();
+
+        // Create a multi-creator submission and set it as the winner
+        address[] memory creatorAddresses = new address[](3);
+        creatorAddresses[0] = address(0x1);
+        creatorAddresses[1] = address(0x2);
+        creatorAddresses[2] = address(0x3);
+
+        uint256[] memory creatorBps = new uint256[](3);
+        creatorBps[0] = 5000; // 50%
+        creatorBps[1] = 3000; // 30%
+        creatorBps[2] = 2000; // 20%
+
+        uint256 pieceId = createContestSubmissionMultiCreator(
+            "Multi-Creator Submission",
+            "A collaborative masterpiece",
+            ICultureIndex.MediaType.IMAGE,
+            "ipfs://multi",
+            "",
+            "",
+            creatorAddresses,
+            creatorBps
+        );
+        // create 2 more pieces
+        uint256 pieceId2 = createContestSubmission(
+            "Second Submission",
+            "Second masterpiece",
+            ICultureIndex.MediaType.IMAGE,
+            "ipfs://second",
+            "",
+            "",
+            address(0x2),
+            10000
+        );
+        uint256 pieceId3 = createContestSubmission(
+            "Third Submission",
+            "Third masterpiece",
+            ICultureIndex.MediaType.IMAGE,
+            "ipfs://third",
+            "",
+            "",
+            address(0x3),
+            10000
+        );
+
+        // Allocate ETH to the contest contract to simulate prize pool using vm.deal
+        uint256 prizePoolAmount = 1 ether;
+        vm.deal(address(baseContest), prizePoolAmount);
+
+        // Fast forward time to after the contest ends
+        vm.warp(baseContest.endTime() + 1);
+
+        // unpause
+        vm.prank(founder);
+        baseContest.unpause();
+
+        // Calculate expected balances based on payout splits
+        uint256 expectedWinner1Balance = (prizePoolAmount * payoutSplits[0]) / 1e6;
+        uint256 expectedWinner2Balance = (prizePoolAmount * payoutSplits[1]) / 1e6;
+        uint256 expectedWinner3Balance = (prizePoolAmount * payoutSplits[2]) / 1e6;
+
+        // Pay out winners by the owner after contest ends
+        vm.prank(founder);
+        vm.expectEmit(false, true, false, true);
+        emit ReceiveETH(creatorAddresses[0], expectedWinner1Balance);
+        vm.expectEmit(false, true, false, true);
+        emit ReceiveETH(creatorAddresses[1], expectedWinner2Balance);
+        vm.expectEmit(false, true, false, true);
+        emit ReceiveETH(creatorAddresses[2], expectedWinner3Balance);
+        baseContest.payOutWinners(3);
+
+        // Assert that contest balance is empty
+        assertEq(address(baseContest).balance, 0, "Contest balance should be empty");
+
+        // assert contest paid out and expect revert if trying to pay out again
+        assertEq(baseContest.paidOut(), true, "Contest should be paid out");
+
+        vm.prank(founder);
+        vm.expectRevert(IBaseContest.CONTEST_ALREADY_PAID_OUT.selector);
+        baseContest.payOutWinners(3);
+
+        // check that cultureindex maxheap.size is 0 after paying out
+        CultureIndex contestIndex = CultureIndex(address(baseContest.cultureIndex()));
+        MaxHeap maxHeap = MaxHeap(address(contestIndex.maxHeap()));
+
+        // ensure baseContest initialBalance is 1 ether
+        assertEq(baseContest.initialBalance(), 1 ether, "Initial balance should be 1 ether");
+
+        // ensure payoutIndex is 1
+        assertEq(baseContest.payoutIndex(), 3, "Payout index should be 1");
     }
 
     event ReceiveETH(address indexed sender, uint256 amount);
