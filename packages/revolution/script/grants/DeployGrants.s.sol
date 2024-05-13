9981be19b557f9177057711a2e12cf8fff6115b8
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-05-13 11:32:39 -0400

    new grants deploy

--- packages/revolution/script/grants/DeployGrants.s.sol
+++ packages/revolution/script/grants/DeployGrants.s.sol
@@ -49,10 +49,11 @@ contract DeployGrants is Script {
     }
 
     function writeDeploymentDetailsToFile(uint256 chainID) private {
-        string memory filePath = string(abi.encodePacked("deploys/", chainID.toString(), ".txt"));
+        string memory filePath = string(abi.encodePacked("deploys/grants/", chainID.toString(), ".txt"));
 
         vm.writeFile(filePath, "");
         vm.writeLine(filePath, string(abi.encodePacked("Grants: ", addressToString(grants))));
+        vm.writeLine(filePath, string(abi.encodePacked("GrantsImpl: ", addressToString(grantsImpl))));
     }
 
     function addressToString(address _addr) private pure returns (string memory) {
